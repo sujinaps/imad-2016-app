a547e322afcb2a983d1a6afb9ad12e7f7918b2ca
sujinaps <sujinaps@gmail.com>
2016-09-29 23:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Article-one',function (req, res) {
-  res.send('Article one is requested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 }); 
   app.get('/Article-two',function (req, res) {
   res.send('Article two is requested and will be served here');
