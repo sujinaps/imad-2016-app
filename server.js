0212b53a72d289d8325dd367f46907b7624b117d
sujinaps <sujinaps@gmail.com>
2016-09-30 23:29:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,13 +25,42 @@ function CreateTemplate(data)
     var content = data.content;
     var htmlTemplate =
     {
-       
- 
+    
+    <html>
+    <head>
+        <title>
+             ${title}
+        </title>
+        
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+
+<body>
+    <div class="container">
+            <div>
+                <a href='/'>home></a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+            
+    </div>
+</body>
+
+</html>
 };
     
 return htmlTemplate;
 }
-
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
