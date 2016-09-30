a078210fb012087841d56e4b462911cd943a0a34
sujinaps <sujinaps@gmail.com>
2016-10-01 00:00:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var Articleone ={
+var Articles = {
+    
+     `Article-one` :{
     title:'Article-one myself Sujina',
     heading :'Article One',
     date: 'Sep 2 2016',
@@ -15,7 +17,42 @@ var Articleone ={
                 </p>
                 <p>
                     This is the Content for Article one This is the Content for Article one`
-}; /* added by me to reduce the html code Module P4*/
+            },
+    `Article-two`:{
+        title:'Article-two myself Sujina',
+        heading :'Article two',
+        date: 'Sep 2 2016',
+        content: `<p>
+                    This is the Content for Article two 
+                </p>
+                <p>
+                    This is the Content for Article two This is the Content for Article two`
+    },
+    `Article-three`:{
+        title:'Article-three myself Sujina',
+        heading :'Article three',
+        date: 'Sep 2 2016',
+        content: `<p>
+                    This is the Content for Article three This is the Content for Article three 
+                </p>
+                <p>
+                    This is the Content for Article three `
+                  
+}
+    
+};
+
+
+/*var Articleone ={
+    title:'Article-one myself Sujina',
+    heading :'Article One',
+    date: 'Sep 2 2016',
+    content: `<p>
+                    This is the Content for Article one This is the Content for Article one This is the Content for Article one This is the Content for Article one This is the Content for Article one
+                </p>
+                <p>
+                    This is the Content for Article one This is the Content for Article one`
+}; /* added by me to reduce the html code Module P4*/ 
 
 function CreateTemplate(data)
 {
@@ -65,9 +102,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-one',function (req, res) {
+//app.get('/Article-one',function (req, res) {
+app.get('/:ArticleName',function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
-  res.send(CreateTemplate(Articleone)); 
+  //res.send(CreateTemplate(Articleone)); 
+  var ArticleName = ewq.params.ArticleName;
+  res.send(CreateTemplate(Articles[ArticleName]));
+  
 }); 
   app.get('/Article-two',function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
