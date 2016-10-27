5489472827e7a35556fa08082024328f135cf58b
sujinaps <sujinaps@gmail.com>
2016-10-27 12:53:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+element.innerHTML -= 'New-Value';
+
+var img = document.getElementById('img');
+img.onclick= function{
+    img.style.marginLeft = '100px';
+};
