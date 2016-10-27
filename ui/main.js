ce154e3cf6207682703dd273606ccd593d42040b
sujinaps <sujinaps@gmail.com>
2016-10-27 13:17:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 var element = document.getElementById('main-text');
-element.innerHTML -= 'New Value';
+element.innerHTML -= 'New-Value';
 
 var img = document.getElementById('modi');
 img.onclick= function(){
