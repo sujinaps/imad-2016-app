fd1fc4a110fff8d48dd674bd23629909bf036872
sujinaps <sujinaps@gmail.com>
2016-10-28 00:06:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,9 @@ img.onclick= function(){
 
 
 //counter code
-var counter =0;
+
 var button = document.getElementById('counter');
+var counter =0;
 button.onclick = function(){
     
     counter = counter+1;
